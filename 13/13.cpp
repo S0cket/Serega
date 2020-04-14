6f9257e53d4cc1b7a24da0e305d498705b1bf3c6
S0cket <rus.swordsman@gmail.com>
2020-04-15 01:40:32 +0400

    commit

---
+++ 13/13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+const int n = 5;
+int a[n][n], count = 0;
+
+int main(void) {
+	srand(time(NULL));
+	for (int i = 0; i < n; i ++) {
+		for (int j = 0; j < n; j ++) {
+			a[i][j] = rand() % 100 - 50;
+			if (abs(a[i][j]) < 20)
+				count ++;
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "count: " << count << endl;
+	return 0;
+}
